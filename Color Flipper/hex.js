ff204d06ad80b0f10cf6cb51d880dc5b856ab240
deepeshaburse <deepesha.3007@gmail.com>
2022-01-22 23:43:26 +0530

    Added code

---
+++ Color Flipper/hex.js
@@ -0,0 +1,12 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    let hexcolor = '#';
+    for(let i = 0; i < 6; i++) {
+        hexcolor += hex[Math.floor(Math.random() * 16)];
+    }
+    color.textContent = hexcolor;
+    document.body.style.backgroundColor = hexcolor;
+})
