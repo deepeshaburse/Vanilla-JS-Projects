5d6cd51b8b59a6a236033899423acf857fa34342
deepeshaburse <deepesha.3007@gmail.com>
2022-01-31 22:31:47 +0530

    Updated app.js

--- Countdown/app.js
+++ Countdown/app.js
@@ -26,7 +26,14 @@ const months = [
   const deadline = document.querySelector(".deadline");
   const items = document.querySelectorAll(".deadline-format h4");
 
-  let futureDate = new Date(2022, 4, 29, 17, 30, 0);
+  let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+// months are ZERO index based;
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+  // let futureDate = new Date(2022, 4, 29, 17, 30, 0);
   
   const year = futureDate.getFullYear();
   const hours = futureDate.getHours();
